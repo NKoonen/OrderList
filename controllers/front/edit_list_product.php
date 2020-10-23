e972cdf8fc2efff2d38a274b795a71bf5434aa2d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-10-23 15:24:02 +0200

    Prepare controller for AJAX

--- controllers/front/edit_list_product.php
+++ controllers/front/edit_list_product.php
@@ -19,6 +19,9 @@ class orderlistedit_list_productModuleFrontController extends ModuleFrontControl
             die();
         }
 
+        $is_ajax = Tools::getValue('ajax');
+        $json    = array();
+
         /**
          * @var \Db $db
          */
@@ -27,15 +30,33 @@ class orderlistedit_list_productModuleFrontController extends ModuleFrontControl
         if (Tools::getValue('delete_list_product')) {
             /** @var bool $result */
 	        $result = $this->deleteListProduct( $product_id, $customer );
+
+	        if ( $is_ajax ) {
+		        $json['content'] = $this->module;
+	        } else {
 		        Tools::redirect(Context::getContext()->link->getModuleLink('orderlist', 'my_list'));
+	        }
 
         } else {
             /** @var bool $result */
             $result = $this->addListProduct( $product_id, $customer );
+
+            if ( $is_ajax ) {
+	            $params = array(
+		            'product' => $product_id,
+	            );
+	            $json['content'] = $this->module->hookDisplayProductAdditionalInfo( $params );
+            } else {
 	            Tools::redirect($this->context->link->getProductLink($product_id));
+            }
         }
 
+        if ( $is_ajax ) {
+        	$json['success'] = ! empty( $result );
+        	echo json_encode( $json );
+        }
 
+	    die();
     }
 
     public function deleteListProduct( $product_id, $customer ) {
